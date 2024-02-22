973004cc0939d245c064d69087652a819a2416db
Sidney Douw <mail@sidneydouw.de>
2024-02-22 09:27:11 +0000

    add adjust_folder_names_and_paths function

--- workflow.py
+++ workflow.py
@@ -1,7 +1,9 @@
+import os
 import uuid
 import time
 import yaml
 import execution
+import folder_paths
 
 
 def run_workflow(workflow_file: str):
@@ -10,7 +12,6 @@ def run_workflow(workflow_file: str):
         valid = execution.validate_prompt(workflow)
 
         if valid[0]:
-            alter_base_path()
 
             prompt_id = str(uuid.uuid4())
             outputs_to_execute = valid[2]
@@ -29,7 +30,28 @@ def run_workflow(workflow_file: str):
             print("node errors:", valid[3])
 
 
-def alter_base_path():
-    base_path = "/home/ubuntu/projects/comfy_ui_cache"
-    # TODO: implement
+# We do this because ComfyUI Manager uses the first path in the `folder_names_and_paths` dictionary
+# to save models and custom nodes
+def adjust_folder_names_and_paths(new_base_path: str):
+    if folder_paths.base_path == new_base_path:
+        return
+
+    for category in folder_paths.folder_names_and_paths:
+        # for every category, replace the base path with the new base path
+        for i, folder in enumerate(folder_paths.folder_names_and_paths[category][0]):
+            # skip output folders added in the main script
+            if folder.startswith(f"{folder_paths.base_path}/output"):
+                continue
+
+            folder_paths.folder_names_and_paths[category][0][i] = folder.replace(
+                folder_paths.base_path, new_base_path
+            )
+
+        # add back in original custom nodes folder for ComfyUI Manager installation
+        if category == "custom_nodes":
+            folder_paths.folder_names_and_paths[category][0].append(
+                os.path.join(folder_paths.base_path, "custom_nodes")
+            )
+
+    print(folder_paths.folder_names_and_paths, "\n")
     return
