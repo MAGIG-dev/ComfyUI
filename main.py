81c091e12aa16c97c7a27478916e1837a1169242
Sidney Douw <mail@sidneydouw.de>
2024-02-22 08:49:22 +0000

    refactor how workflow only gets executed

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ import threading
 import gc
 
 from comfy.cli_args import args
-from workflow import run_workflow_only
+from workflow import run_workflow
 
 if os.name == "nt":
     import logging
@@ -186,17 +186,18 @@ def load_extra_path_config(yaml_path):
                 folder_paths.add_model_folder_path(x, full_path)
 
 
-def run_with_server():
+if __name__ == "__main__":
     if args.temp_directory:
         temp_dir = os.path.join(os.path.abspath(args.temp_directory), "temp")
         print(f"Setting temp directory to: {temp_dir}")
         folder_paths.set_temp_directory(temp_dir)
     cleanup_temp()
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    server = PromptServer(loop)
-    q = execution.PromptQueue(server)
+    if not args.workflow:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        server = PromptServer(loop)
+        q = execution.PromptQueue(server)
 
     extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
     if os.path.isfile(extra_model_paths_config_path):
@@ -210,10 +211,11 @@ def run_with_server():
 
     cuda_malloc_warning()
 
-    server.add_routes()
-    hijack_progress(server)
+    if not args.workflow:
+        server.add_routes()
+        hijack_progress(server)
 
-    threading.Thread(target=prompt_worker, daemon=True, args=(q, server,)).start()
+        threading.Thread(target=prompt_worker, daemon=True, args=(q, server,)).start()
 
     if args.output_directory:
         output_dir = os.path.abspath(args.output_directory)
@@ -233,25 +235,21 @@ def run_with_server():
     if args.quick_test_for_ci:
         exit(0)
 
-    call_on_start = None
-    if args.auto_launch:
-        def startup_server(address, port):
-            import webbrowser
-            if os.name == 'nt' and address == '0.0.0.0':
-                address = '127.0.0.1'
-            webbrowser.open(f"http://{address}:{port}")
-        call_on_start = startup_server
+    if args.workflow:
+        run_workflow(args.workflow)
+    else:
+        call_on_start = None
+        if args.auto_launch:
+            def startup_server(address, port):
+                import webbrowser
+                if os.name == 'nt' and address == '0.0.0.0':
+                    address = '127.0.0.1'
+                webbrowser.open(f"http://{address}:{port}")
+            call_on_start = startup_server
 
-    try:
-        loop.run_until_complete(run(server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
-    except KeyboardInterrupt:
-        print("\nStopped server")
+        try:
+            loop.run_until_complete(run(server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
+        except KeyboardInterrupt:
+            print("\nStopped server")
 
     cleanup_temp()
-
-
-if __name__ == "__main__":
-    if args.workflow:
-        run_workflow_only()
-    else:
-        run_with_server()
